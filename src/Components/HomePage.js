29bd464f843240f31e6698a1530c41f6f21d454d
Sandip Adhikari <sandipad111@gmail.com>
2024-05-25 13:16:37 +0545

    buttons added

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import PlayGame from './PlayGame';
 
 function HomePage() {
   return (
     <div className="App">
       <header className="App-header">
         <p>This is HomePage</p>
+        <PlayGame />
       </header>
     </div>
   );
