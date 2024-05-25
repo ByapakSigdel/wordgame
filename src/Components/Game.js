dbe4d800c6796f5935d5d0bbd76231187d9d195c
ByapakSigdel <sigdelmb123@gmail.com>
2024-05-25 21:24:05 +0545

    Made the text section and made the api call successful

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import TextBox from './TextBox';
+import UserGrid from './UserGrid';
 
 function Game() {
   return (
     <div className="App">
       <header className="App-header">
         <p>GamePage</p>
+        
+        <UserGrid />
+        <TextBox />
       </header>
     </div>
   );
