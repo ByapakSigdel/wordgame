2d979a287cbedf9de42086db31d73ccfb824ad2f
ByapakSigdel <sigdelmb123@gmail.com>
2024-05-25 12:37:48 +0545

    Made changes to route paths and files

---
+++ src/Components/Game.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Game() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>GamePage</p>
+      </header>
+    </div>
+  );
+}
+
+export default Game;
---
+++ src/Components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function HomePage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This is HomePage</p>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
