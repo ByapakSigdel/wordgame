13fa23180dee6cf0ebfa33b30a61d068cf2b341f
ByapakSigdel <sigdelmb123@gmail.com>
2024-05-25 13:14:16 +0545

    Installed the tailwind library

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -5,7 +5,7 @@ function HomePage() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>This is HomePage</p>
+      <p className="ml-4 text-blue-500 text-2xl">This is HomePage</p>
       </header>
     </div>
   );
